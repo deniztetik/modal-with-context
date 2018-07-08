c58c23140b85f8695b6e7c1e6025fe64509e7c42
Deniz Tetik <deniz.tetik@oracle.com>
2018-07-08 01:16:36 -0400

    Modularize out context specific functionality

--- src/ModalContainer.js
+++ src/ModalContainer.js
@@ -2,8 +2,45 @@ import React, { Component } from 'react';
 
 const ModalContext = React.createContext();
 
-export const withModalProvider = (WrappedComponent) => {
+const state = {
+  isOpen: false,
+};
+
+const actions = new Map([
+  ['openModal', { isOpen: true }],
+  ['closeModal', { isOpen: false }],
+]);
+
+export const ExtraComponent = ({ isOpen, closeModal }) => (
+  isOpen ?
+    <div id='app-modal'>
+      <div id='app-modal__content'>
+        <h1>This is a modal</h1>
+        <button
+          onClick={closeModal}
+        >
+          Close Modal
+      </button>
+      </div>
+    </div> : null
+);
+ 
+const createModalProvider = (actionTuples) => (state) => (ExtraComponent) => (WrappedComponent) => {
   return class extends Component {
+    state = state;
+
+    actions = [...actionTuples].reduce((actions, actionTuple) => {
+      const updatedActions = { ...actions };
+      const key = actionTuple[0];
+      const value = actionTuple[1];
+
+      updatedActions[key] = () => {
+        this.setState({ ...value });
+      };
+
+      return updatedActions;
+    }, {});
+
     buildContextStateAndActions() {
       return {
         ...this.state,
@@ -11,42 +48,18 @@ export const withModalProvider = (WrappedComponent) => {
       };
     }
 
-    state = {
-      isOpen: false,
-    }
-
-    actions = {
-      openModal: () => {
-        this.setState({
-          isOpen: true
-        });
-      },
-    }
-
     render() {
-      const {
-        isOpen,
-      } = this.state;
-
       return (
         <ModalContext.Provider value={this.buildContextStateAndActions()}>
           <WrappedComponent {...this.props} />
-          {isOpen ?
-            <div id='app-modal'>
-              <div id='app-modal__content'>
-                <h1>This is a modal</h1>
-                <button
-                  onClick={() => this.setState({ isOpen: false })}
-                >
-                  Close Modal
-                </button>
-              </div>
-            </div> : null}
+          {ExtraComponent ? <ExtraComponent {...this.buildContextStateAndActions()} /> : null}
         </ModalContext.Provider>
       );
     }
   }
-};
+}
+
+export const withModalProvider = createModalProvider(actions)(state)(ExtraComponent);
 
 export const withModalConsumer = (WrappedComponent) =>
   (props) => {
